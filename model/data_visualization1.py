e63895f0cde46f430886c0897ac51aaf2e6bdf67
ilqababon <b2026ilqababon@pshs.edu.ph>
2025-02-03 11:04:44 +0800

    bug fixes

--- model/data_visualization1.py
+++ model/data_visualization1.py
@@ -13,22 +13,23 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 print("BEGIN")
 
-# https://medium.com/@mike.roweprediger/using-pytorch-to-train-an-lstm-forecasting-model-e5a04b6e0e67
 # Load the data
 column_names = ['date', 'YEAR', 'MONTH', 'DAY', 'RAINFALL', 'TMAX', 'TMIN', 'TMEAN', 'WIND_SPEED', 'WIND_DIRECTION', 'RH', 'dengue', 'Cases', 'searches']
 mdata = pandas.read_csv('data/historical/csv_files/final.csv', names=column_names, header=0)
 mdata.head()
 
-mdata["monthday"] = mdata['MONTH'].map(str) + mdata['DAY'].map(str)
+mdata['DAY'] = mdata['DAY'].apply(lambda x: '{0:0>2}'.format(x))
+mdata["monthday"] = ("0" + mdata['MONTH'].map(str) + "." + mdata['DAY'].map(str)).map(float)
 print(mdata['monthday'])
 
 group_by_both = mdata.groupby(["YEAR", "monthday"]).agg({'Cases': 'sum'})
-mdata['YEAR'] = pandas.Categorical(mdata['YEAR'], categories=pandas.unique(mdata['YEAR']))
-mdata['monthday'] = pandas.Categorical(mdata['monthday'], categories=pandas.unique(mdata['monthday']))
 heatmap1_data = pandas.pivot_table(group_by_both, values='Cases',
                      index='YEAR',
                      columns='monthday')
 print(heatmap1_data)
 
 sns_plot = seaborn.heatmap(heatmap1_data, cmap="YlGnBu")
+
+plt.xlabel("Month-Day")
+plt.ylabel("Year")
 plt.show()
